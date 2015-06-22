52b3abddb94194e329eaf2889b5600e6a9128fdf
ENR <e.natter@utb.fr>
2015-06-22 14:17:46 +0200

    WIP : random start delay

--- Prog/main.c
+++ Prog/main.c
@@ -10,18 +10,26 @@ __CONFIG(FOSC_INTOSCIO & MCLRE_ON & WDTE_OFF & PWRTE_ON); // & MCLRE_OFF
 #define TRUE 0xFF
 
 #define SWITCH_ON RB0
+#define RAND_SEED_ADDR_LSB 0
+#define RAND_SEED_ADDR_MSB 1
 
 
 
 void init(){	
-
 	TRISA=0b00111111; //RA6 & RA7 as outputs
 	TRISB=0b11111111;
 	PCON = 0b00001000; //4 MHz internal oscillator
 
 	T1CON = 0b00110000; //Setup Timer 1 with prescale 8
 
+	//Load last saved random seed
+	unsigned int savedSeed = ((unsigned int)eeprom_read(RAND_SEED_ADDR_MSB)) << 8 | eeprom_read(RAND_SEED_ADDR_LSB);
+	srand(savedSeed);
 
+	savedSeed ++;
+	unsigned char* pSavedSeed = (unsigned char*)&savedSeed;
+	eeprom_write(RAND_SEED_ADDR_LSB, pSavedSeed[0]);
+	eeprom_write(RAND_SEED_ADDR_LSB, pSavedSeed[1]);
 }
 
 void goIn(){
@@ -36,15 +44,14 @@ void stop(){
 	PORTA = (PORTA | 0b11000000);
 }
 
-
 void delay(unsigned int delay){
-
 	TMR1IF = 0;
 	TMR1 = delay;
 	TMR1IE = 1; //Activate Timer1 interrupts
 	GIE = 1; //Activate global interrupts
 
 	TMR1ON = 1; //Starts timer
+
 	while(!TMR1IF); //Wait for timer interrupt
 
 	TMR1ON = 0;
@@ -58,12 +65,9 @@ void main(void){
 
 	stop();
 
-	//srand(15);
-
-	int delay_ms = 1;//rand();
+	int delay_ms = (rand() / 32767.0) * 5000.0;  //Get random number between 0 and 5000
 
 	delay(delay_ms);
-
 
 	while (1){
 
